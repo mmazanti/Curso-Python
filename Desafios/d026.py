8234ab2f9fbca7d7ac202b18afe2130c790964ba
mmazanti <math.mazanti@gmail.com>
2024-03-04 16:46:03 -0300

    d026 e d027
    d026 - Conta quantas letras 'a' aparecem em uma frase, sua posição inicial e final.
    d027 - Lê o nome de uma pessoa e mostra o primeiro e último nome.

---
+++ Desafios/d026.py
@@ -0,0 +1,3 @@
+frase = input('Digite uma frase: ')
+print(frase.find(0::'a'))
+print(frase.count('a'))
---
+++ Desafios/d027.py
@@ -0,0 +1,2 @@
+nome = input('Digite um nome completo: ')
+print('O nome é {}. \nO primeiro nome é {} e o último nome é {}'.format(nome, nome.split([0]), nome.split([3])))
